32a8b7d2e564544efdaefbd1a0a9458e3b6f9877
CX-Yang <CXYang17@gmail.com>
2023-07-19 15:14:04 -0500

    refactored app.js

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ const ejs = require('ejs');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
-//grabs the body content
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
 const cardListUGL = [];
 const cardListUST = [];
 const cardListUND = [];
+
+//Sets pages up for grabbing content and renders home page
+function setUp() {
+    app.set('view engine', 'ejs');
+
+    //grabs the body content
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static("public"));
+
+    app.get("/", (req, res) => {
+        res.render("home");
+    });
+}
 
 function urlFormat(word) {
     return word.toLowerCase().split(" ").join("-");
@@ -21,90 +28,84 @@ function urlFormat(word) {
 
 //refer to cards in sets: Unglued (UGL), Unstable (UST), and Unsanctioned (UND), Unifity?
 //store card info on load
+function setCards(setName, setArr) {
+    mtg.card.all({ set: setName }).on('data', card => {
+        setArr.push(card);
+    });
+}
+
+//Takes the route currently on and then redirects to card searched page
+function handleRedirectSearch(route) {
+    app.post(route, (req, res) => {
+        const cardSearch = req.body.searchCard;
+        res.redirect("/card/" + urlFormat(cardSearch));
+    });
+}
+
+//Takes the route and card set name and renders the current set onto a new page
+function handleSetRender(route, setName) {
+    app.get(route, (req, res) => {
+        res.render("unSet", { cardList: cardListUGL, setName: setName });
+    });
+}
+
+//Search for a card by name with the search bar
+function findCardByName() {
+    app.get("/card/:cardName", (req, res) => {
+        //url check and converted so we can search for the actual card
+        const cardTitle = req.params.cardName;
+        const cardSearch = cardTitle.split("-").join(" ");
+
+        //search for card name
+        mtg.card.where({ name: cardSearch })
+            .then(cards => {
+                //filter out the array of cards
+                const card = cards.filter(card => cardSearch === card.name.toLowerCase());
+                if (card.length < 1) {
+                    res.render("cardError");
+                } else {
+                    res.render("card", { wantedCard: card[0] })
+                }
+            });
+    });
+}
+
+function openPort() {
+    app.listen("3000", () => {
+        console.log("Listening on port 3000");
+    });
+}
+
+//Set up resources needed 
+setUp();
+
 //UGL
-mtg.card.all({ set: "UGL" }).on('data', card => {
-    cardListUGL.push(card);
-});
+setCards("UGL", cardListUGL);
 
 //UST
-mtg.card.all({ set: "UST" }).on('data', card => {
-    cardListUST.push(card);
-});
+setCards("UST", cardListUST);
 
 //UND
-mtg.card.all({ set: "UND" }).on('data', card => {
-    cardListUND.push(card);
-});
-
-//Set up the home page
-app.get("/", (req, res) => {
-    res.render("home");
-});
-
-//Card search redirect
-app.post("/", (req, res) => {
-    const cardSearch = req.body.searchCard;
-    res.redirect("/card/" + urlFormat(cardSearch));
-});
+setCards("UND", cardListUND);
 
 //Unglued set
-app.get("/unglued", (req, res) => {
-    res.render("unSet", { cardList: cardListUGL, setName: "ugl" });
-});
-
-//Card search redirect
-app.post("/unglued", (req, res) => {
-    const cardSearch = req.body.searchCard;
-    res.redirect("/card/" + urlFormat(cardSearch));
-});
+handleSetRender("/unglued", "ugl");
 
 //Unstable set
-app.get("/unstable", (req, res) => {
-    res.render("unSet", { cardList: cardListUST, setName: "ust" });
-});
-
-//Card search redirect
-app.post("/unstable", (req, res) => {
-    const cardSearch = req.body.searchCard;
-    res.redirect("/card/" + urlFormat(cardSearch));
-});
+handleSetRender("/unstable", "ust")
 
 //Unsactionedd set
-app.get("/unsanctioned", (req, res) => {
-    res.render("unSet", { cardList: cardListUND, setName: "und" });
-});
+handleSetRender("/unsanctioned", "und");
+
+//Get specific card
+findCardByName();
 
 //Card search redirect
-app.post("/unsanctioned", (req, res) => {
-    const cardSearch = req.body.searchCard;
-    res.redirect("/card/" + urlFormat(cardSearch));
-});
+handleRedirectSearch("/");
+handleRedirectSearch("/unglued");
+handleRedirectSearch("/unstable");
+handleRedirectSearch("/unsanctioned");
+handleRedirectSearch("/card/:cardName");
 
-
-//get specific card - check 282
-app.get("/card/:cardName", (req, res) => {
-    //url check and converted so we can search for the actual card
-    const cardTitle = req.params.cardName;
-    const cardSearch = cardTitle.split("-").join(" ");
-
-    //search for card name
-    mtg.card.where({ name: cardSearch })
-        .then(cards => {
-            //filter out the array of cards
-            const card = cards.filter(card => cardSearch === card.name.toLowerCase());
-            if (card.length < 1) {
-                res.render("cardError");
-            } else {
-                res.render("card", { wantedCard: card[0] })
-            }
-        });
-});
-
-app.post("/card/:cardName", (req, res) => {
-    const cardSearch = req.body.searchCard;
-    res.redirect("/card/" + urlFormat(cardSearch));
-})
-
-app.listen("3000", () => {
-    console.log("Listening on port 3000");
-});
+//Listen to port 3000
+openPort();
